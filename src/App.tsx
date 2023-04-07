8425f0bc4f7c9df75df9c0aeef9618cf7217aea3
Ebenezer Aladesuyi <ebenezeraladesuyi@gmail.com>
2023-04-07 15:15:52 +0100

    route-update

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,11 @@ function App() {
   <>  
 
       <div>
-        {/* { getUser?.name ?  */}
-        {/* <DashBoardRoute />  */}
-        {/* // : */}
+        { getUser?.name ?  
+       <DashBoardRoute />  
+         :
                  <HomeRoutes /> 
-        {/* } */}
+        }
       </div>
 
       <div>
